73c72c47091c3aebfb441ef2290c4571f01eaf6a
Aayush Vyas <aayush25vyas@gmail.com>
2025-10-21 23:23:33 +0530

    Notfound Page was also implemented

--- frontend/src/pages/NotFound/NotFoundPage.jsx
+++ frontend/src/pages/NotFound/NotFoundPage.jsx
@@ -1,5 +1,21 @@
+import { MainLayout } from "../../layout";
+import { Box, Heading, Text, VStack, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+
 function NoteFoundPage(){
-    return <>Not Found Page</>
+    return <MainLayout>
+          <VStack spacing={4} justify="center" m={10}>
+          <Heading as="h1" size="2xl">
+            404
+          </Heading>
+          <Text fontSize="xl">Page Not Found</Text>
+          <Text>The page you are looking for does not exist.</Text>
+          <Link to="/">
+            <Button colorScheme="teal">Go to Homepage</Button>
+          </Link>
+        </VStack>
+    </MainLayout>
 }
 
 export default NoteFoundPage; 
