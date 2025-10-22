ffee685433f8b7b79a4ccb9256e04e579700bc64
Aayush Vyas <aayush25vyas@gmail.com>
2025-10-22 12:11:49 +0530

    Fix the MODE Problem for deployement

--- frontend/src/helper/constant/envVariables.js
+++ frontend/src/helper/constant/envVariables.js
@@ -1,5 +1,8 @@
 const envVariables = {
-    backendBaseUrl:import.meta.env.VITE_BACKEND_URL,
-}; 
+  backendBaseUrl:
+    import.meta.env.VITE_MODE === "development"
+      ? import.meta.env.VITE_BACKEND_URL
+      : "/api/v1",
+};
 
-export default envVariables; 
+export default envVariables;
